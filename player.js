e0aa3a1dc363e4a62be30096479576db2cd1dea9
wangriyu <wangriyu1997@gmail.com>
2018-12-01 19:48:21 +0800

    add localStorage

--- player.js
+++ player.js
@@ -9,27 +9,51 @@
     fixed: !0,
   });
   var xhr = new XMLHttpRequest(), listid = 883067320;
-  xhr.open('get', 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=' + listid),
-  (xhr.onreadystatechange = function() {
-      4 === xhr.readyState &&
-        (200 === xhr.status
-          ? (ap.list.add(JSON.parse(xhr.responseText)), ap.play())
-          : (ap.list.add({
-              title: 'Fell For U',
-              author: 'Noicybino',
-              url: 'FellForU.mp3',
-              pic: 'img/love.png',
-              lrc: '',
-            }),
-            ap.play()));
-  }),
-  xhr.send();
+
+  if (localStorage.getItem('aplayer-list')) {
+    var list = JSON.parse(localStorage.getItem('aplayer-list'));
+    ap.list.add(list);
+    ap.play();
+
+    xhr.open('get', 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=' + listid);
+    xhr.onreadystatechange = function() {
+      if (4 === xhr.readyState) {
+        if (200 === xhr.status) {
+          var newList = JSON.parse(xhr.responseText);
+          (newList.length != list.length) && localStorage.setItem('aplayer-list', xhr.responseText)
+        }
+      }
+    }
+    xhr.send();
+  } else {
+    xhr.open('get', 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=' + listid);
+    xhr.onreadystatechange = function() {
+      if (4 === xhr.readyState) {
+        if (200 === xhr.status) {
+          var list = JSON.parse(xhr.responseText);
+          ap.list.add(list);
+          ap.play();
+          localStorage.setItem('aplayer-list', xhr.responseText)
+        } else {
+          ap.list.add({
+            title: 'Fell For U',
+            author: 'Noicybino',
+            url: 'FellForU.mp3',
+            pic: 'img/love.png',
+            lrc: '',
+          })
+          ap.play()
+        }
+      }
+    }
+    xhr.send();
+  }
 
   document.getElementById('previous').addEventListener('click', function () {
     ap.skipBack()
   });
   document.getElementById('next').addEventListener('click', function () {
-    ap.skipBack()
+    ap.skipForward()
   });
   ap.on('play', function() {
     document.getElementById('avt').innerHTML='<img id="fish" src="/img/avatar.jpg">'
